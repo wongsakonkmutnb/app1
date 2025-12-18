6adceae51339344e90124f1b1a6d1d703a275964
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2025-12-18 11:58:07 +0700

    Update app.js with css inline

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <table border="1">
-      <tr>
-        <th>Product</th><th>Price</th>
-      </tr>
-      <tr>
-        <td>React</td><td>320</td>
-      </tr>
-      <tr>
-        <td>React Native</td><td>340</td>
-      </tr>
-    </table>
-  );
+  const divStyle={
+    color:'red',
+    backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'3px',
+  };
+  return <div style={divStyle}>Hello react</div>
 }
 
 export default App;
