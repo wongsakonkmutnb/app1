93e49d00ce0bae7b7e597224e9de73faec6b2764
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2025-12-25 09:45:07 +0700

    External

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
-  const divStyle={
-    color:'red',
-    backgroundColor:'powderblue',
-    fontSize:'larger',
-    padding:'3px',
-  };
-  return <div style={divStyle}>Hello react</div>
+  //const divStyle={
+  //  color:'red',
+  //  backgroundColor:'powderblue',
+  //  fontSize:'larger',
+  //  padding:'3px',
+  //};
+  //return <div style={divStyle}>Hello react</div>
+  return (
+    <>
+    <h2 >DeveloperThai.com</h2>
+    <div className="title">React & React Native</div>
+    </>
+  )
 }
 
 export default App;
