7c1dc378d645aa295c6add6c947c506639dad1fa
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2025-12-25 11:02:08 +0700

    Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-//import logo from './logo.svg';
-//import './App.css';
 import React from 'react';
-import './style.css';
+import { Header , Content , Footer } from './func-components';
+
 
 function App() {
-  //const divStyle={
-  //  color:'red',
-  //  backgroundColor:'powderblue',
-  //  fontSize:'larger',
-  //  padding:'3px',
-  //};
-  //return <div style={divStyle}>Hello react</div>
+
   return (
-    <>
-    <h2 >DeveloperThai.com</h2>
-    <div className="title">React & React Native</div>
-    </>
+    [<Header/>, <Content/>, <Footer/>]
   )
 }
 
---
+++ src/func-components.js
@@ -0,0 +1,34 @@
+import react from 'react';
+
+export function Header(){//หัว
+    const aStyle={
+        display:'inline-block',
+        color:'blue',
+        margin:'7px',
+        textDecoration:'none'
+    }
+    return (
+        <div style={{backgroundColor:'#ccc'}}>
+            <a href="" style={aStyle}>Home</a> |&nbsp;
+            <a href="" style={aStyle}>React</a> |&nbsp;
+            <a href="" style={aStyle}>React Native</a>
+        </div>
+    )
+}//จบหัว
+
+export function Content(){//
+    return(
+        <div style={{textAlign:'center'}}>
+            <h2>Hello<br/>React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
+}//
+
+export const Footer = () => {//ล่าง
+    return (
+        <div style={{textAlign:'center'}}>
+            &copy; {new Date().getFullYear()} All rights reserved
+        </div>
+    )
+}//จบล่าง
