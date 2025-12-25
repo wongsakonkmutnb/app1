6c705658fceb7c2f5c8d7ff5180c8d90752b8cbf
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2025-12-25 12:01:04 +0700

    class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Header , Content , Footer } from './func-components';
-
+import Calendar from './class-components';
 function App() {
 
   return (
-    [<Header/>, <Content/>, <Footer/>]
+    <>
+    <Header/>
+    <p><center><Calendar/></center></p>
+    <Content/>
+    <Footer/>
+    </>
   )
 }
 
