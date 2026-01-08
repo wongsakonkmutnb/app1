5ca3a931084014c04a6a8636d8b3f3e01e3249f5
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2026-01-08 11:13:37 +0700

    Parameter in Event Handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Button from './class-components';
+import { Calculator2 } from "./calculator";
 
 
 function App() {
-  return <Button/>
+  return <Calculator2/>
 }
 
 export default App;
