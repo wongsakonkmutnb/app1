cc57a86cb561ef6fbb52d9cda68c944e8e08928c
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2026-01-15 11:26:12 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import RefsArray from "./refs-array";
+import MessageBox from "./state-func";
 
 
 
 function App() {
-  return <RefsArray/>
+  return <MessageBox/>
 }
 
 export default App;
