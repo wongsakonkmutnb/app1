736eb5714dabf8294c6973a1604f702d79d5abab
wongsakonkmutnb <s6703052411121@email.kmutnb.ac.th>
2026-02-12 10:02:23 +0700

    Navlink

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-
-import { userContext } from "./context";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
+import Router2 from "./router2";
 
 function App() {
-  let [user, setUser] = React.useState('');
-  return(
-    <userContext.Provider value={[user, setUser]}>
-      <Header2 />
-      <Content2 />
-    </userContext.Provider>
+  return <Router2 />
 
-
-
-  )
 }
 export default App;
